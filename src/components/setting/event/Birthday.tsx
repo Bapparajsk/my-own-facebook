36ff002934a7b2b5c5094a23ae54eee0123f606b
Bapparaj sk <bapparajsk97@gmail.com>
2024-09-05 23:28:31 +0530

    refactor: Include lazy loading for images in Birthday component and update related code

--- src/components/setting/event/Birthday.tsx
+++ src/components/setting/event/Birthday.tsx
@@ -6,6 +6,9 @@ import { Card, DatePicker, CardBody, CardHeader, Button } from "@nextui-org/reac
 import { parseDate } from "@internationalized/date";
 import { useRouter } from 'next/navigation'
 import { UserSType } from "@/interface/usertupe";
+import { useMutation } from "@tanstack/react-query";
+import { updateBirthday } from "@/lib/credential";
+import { useToasterContext } from "@/context/ToasterContext";
 
 const getDateString = (day: number, month: number, year: number) => {
     return `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`
@@ -16,8 +19,8 @@ export const Birthday = ({ user }: { user: UserSType }) => {
     const [dateChangeValue, setDateChangeValue] = useState<{day: number, month: number, year: number}>();
     const [isChange, setIsChange] = useState<boolean>(false);
 
-    const [] = useState<boolean>(false);
     const router = useRouter();
+    const { setNotyDetails } = useToasterContext();
 
     const { day, month, year } = user.dateOfBirth;
     const Birthdays = getDateString(day, month, year);
@@ -35,14 +38,33 @@ export const Birthday = ({ user }: { user: UserSType }) => {
             return;
         }
 
-        const newdate = getDateString(day, month, year);
+        const newDate = getDateString(day, month, year);
         setDateChangeValue({day, month, year});
-        if (newdate !== Birthdays) {
+        if (newDate !== Birthdays) {
             setIsChange(true);
         } else {
             setIsChange(false);
         }
     }
+
+    const birthMutation = useMutation({
+        mutationFn: async () => {
+            if (!dateChangeValue) {
+                setNotyDetails({ type: "error", contain: { message: "Please select a date" } });
+                return;
+            }
+
+            const { day, month, year } = dateChangeValue;
+            await updateBirthday(day, month, year);
+        },
+        onError: (error) => {
+            setNotyDetails({ type: "error", contain: { message: error.message } });
+        },
+        onSuccess: () => {
+            setNotyDetails({ type: "success", contain: { message: "Date of birth updated successfully" } });
+            router.back();
+        }
+    });
 
     return (
         <Card className={'w-full max-w-[500px] px-5 py-2 flex flex-col bg-transparent/50 backdrop-blur-[5px]'}>
@@ -69,6 +91,8 @@ export const Birthday = ({ user }: { user: UserSType }) => {
                         onPress={() => {
                             if (isChange) {
                                 // TODO: date upload in server
+
+
                                 setIsDisabledDatePicker(true);
                                 setIsChange(false);
                             } else if (isDisabledDatePicker) {
--- src/lib/credential.ts
+++ src/lib/credential.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { isValidEmail } from "./utils";
 
 export const verifyEmail = async (inputEmail: string) => {
     const url = process.env.NEXT_PUBLIC_SERVER_URL;
@@ -38,4 +37,23 @@ export const verifyOtp = async (otp: string): Promise<{ email: string }> => {
     );
 
     return { email: res.data.email };
+};
+
+export const updateBirthday = async (day: number, month: number, year: number) => {
+    const url = process.env.NEXT_PUBLIC_SERVER_URL;
+    const token = localStorage.getItem('app-token');
+
+    if (!url || !token) {
+        throw new Error("Internal App error");
+    }
+
+    await axios.post(
+        `${url}/api/add/change-date-of-birth`,
+        { day, month, year },
+        {
+            headers: {
+                token: token
+            }
+        }
+    );
 };
